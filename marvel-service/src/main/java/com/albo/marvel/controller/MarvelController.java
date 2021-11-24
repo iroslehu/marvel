1bfb6b0d6a053a1d180bd0beb817819d41c3e856
Irving Oswaldo Leyva Humaran <irvingleyva@grupovidanta.com>
2021-11-24 10:30:39 -0600

    Se le agrega al controlador los servicios creados para el manejo de entrada y salida de los datos

--- marvel-service/src/main/java/com/albo/marvel/controller/MarvelController.java
+++ marvel-service/src/main/java/com/albo/marvel/controller/MarvelController.java
@@ -1,13 +1,39 @@
 package com.albo.marvel.controller;
 
+import com.albo.marvel.exceptionhandling.CustomException;
+import com.albo.marvel.service.MarvelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MarvelController {
 
+	private MarvelService marvelService;
+
+	@Autowired
+	public MarvelController(MarvelService marvelService){
+		this.marvelService = marvelService;
+	}
+
 	@RequestMapping("/")
 	public String home(){
 		return "Application runing!";
 	}
+
+	@GetMapping("/marvel/sync")
+	public String sync() throws CustomException {
+		return marvelService.sync() ? "Data Synced" : "Data not Synced";
+	}
+
+	@GetMapping("/marvel/colaborators/{name}")
+	public boolean getAllColaborators(@PathVariable String name) throws CustomException {
+		return marvelService.getMarvelColaborators(name);
+	}
+
+	@GetMapping("/marvel/characters/{name}")
+	@ExceptionHandler({ CustomException.class})
+	public boolean getAllCharacters(@PathVariable String name)  throws CustomException {
+		return marvelService.getMarvelCharacters(name);
+	}
+
 }
